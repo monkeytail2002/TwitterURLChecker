7c87c50be5bf3109def8b041e3b5247eb6da6a5d
Angus MacDonald <43272904+monkeytail2002@users.noreply.github.com>
2019-11-14 15:14:17 +0000

    Update testURLscan.py
    Changed script to now display scan information rather than a response.  This provided useful information that will come into use much later when the tool is developed properly.

--- Test Scripts/testURLscan.py
+++ Test Scripts/testURLscan.py
@@ -17,13 +17,15 @@ headers = {
     'API-Key': api,
 }
 
+
 #URL hardcoded in the data.  It is a dodgy URL so don't use it outside of this test
 #url = url, public = public scan through urlscan.io, tags are set to highlight if phishing or malicious
-data = '{"url": "http://landiel.xyz/r.php?t=c&d=89185&l=5548&c=145298", "public": "on","tags": ["phishing", "malicious"]}'
+data = '{"url":"www.google.com" , "public": "on"}'
 
 #Posts scan through urlscan.io with headers and data
-response = requests.post('https://urlscan.io/api/v1/scan/', headers=headers, data=data)
+scan = requests.post('https://urlscan.io/api/v1/scan/', headers=headers, data=data)
 
-#Prints api for test purposes, should see a response(200) if successful
-scan = response
-print(scan)
+#decodes scan details.  This should result in the UUID printing to console.
+scandetails = scan.content.decode('utf-8')
+#Prints scan details.
+print(scandetails)
