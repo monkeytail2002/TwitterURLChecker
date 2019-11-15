4c13465bc5d93560a8ce7596494850a8f40a3c1a
Angus MacDonald <43272904+monkeytail2002@users.noreply.github.com>
2019-11-15 13:41:05 +0000

    Add files via upload
    Test script to get a scan to feed into the request and return the results.

---
+++ Test Scripts/testrequest.py
@@ -0,0 +1,45 @@
+#!/user/bin/python
+import configparser
+import requests
+import json
+import time
+#read config file for API key
+config = configparser.ConfigParser()
+config.sections()
+config.read('../TwitterScrape/credentials.ini')
+
+api = config.get("keys", 'urlapi')
+
+#Set headers and data for api usage
+headers = {
+	'Content-Type': 'application/json',
+	'API-Key': api,
+}
+
+data = '{"url":"http://bestravelways.com/P1C0uUXVxpq.jsv?byuIqrLNbdSJ=PszfbaUhtspk18d9brJ032bju01farr0116612056ozcw2fio", "public": "on"}'
+
+
+#sumbits scan and decodes the details#
+scan = requests.post('https://urlscan.io/api/v1/scan/', headers=headers, data=data)
+
+scandetails = scan.content.decode('utf-8')
+
+#parse the returned json details
+scanjson = json.loads(scandetails)
+
+#test details
+#print(scanjson["uuid"])
+js = scanjson["uuid"]
+#requests the result of the search
+
+uuid = js
+#print(uuid)
+base_url = "https://urlscan.io/api/v1/result/" + str(uuid)
+
+time.sleep(60)
+response = requests.get(base_url)
+print(response)
+
+responsedetails = response.content.decode('utf-8')
+
+print(responsedetails)
