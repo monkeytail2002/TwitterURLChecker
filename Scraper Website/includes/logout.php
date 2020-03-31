55c6e3242b4c842d85e6313330c82e41257abbe1
Angus MacDonald <43272904+monkeytail2002@users.noreply.github.com>
2020-03-31 17:42:45 +0100

    Add files via upload
    Tidied up formatting and added commentary

--- Scraper Website/includes/logout.php
+++ Scraper Website/includes/logout.php
@@ -1,16 +1,23 @@
-<?php 
+<!--
+15009351 Angus MacDonald
+Tutor Suzanne Irvine
+31/3/2020
+-->
+
+<?php
 
 //Destroys sessions and resets cookie
-session_start();
-$_SESSION = array();
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
+    session_start();
+    $_SESSION = array();
+
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
         $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-    );
-}
-setcookie ("Current_user", "", time() - 3600); 
-session_destroy();
+        $params["secure"], $params["httponly"]);
+    }
+    
+    setcookie ("Current_user", "", time() - 3600); 
+    session_destroy();
 
 ?>
