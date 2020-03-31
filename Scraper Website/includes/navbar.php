71bbae71d874ef904c69ebb6f90b89e82eed9fb8
Angus MacDonald <43272904+monkeytail2002@users.noreply.github.com>
2020-03-31 17:44:18 +0100

    Add files via upload
    Tidied formatting, removed if signed out section and added commentary.

--- Scraper Website/includes/navbar.php
+++ Scraper Website/includes/navbar.php
@@ -1,3 +1,10 @@
+<!--
+15009351 Angus MacDonald
+Tutor Suzanne Irvine
+31/3/2020
+-->
+
+
 <div class="navbar">
 
 	<?php
@@ -6,13 +13,14 @@
 	$logged = $_SESSION["UserName"];
 //Checks if user is logged in
 	if($logged == true){
+        
 		?>
 	
 		<div class="logo_div">
-			<a href="Main.php"><h1>Security Suite</h1></a>
-			<span ontouchstart="openTouch(e)" onclick="openNav()"> 
-				<div class="navbg">
-					<div class="bar1"></div>
+            <a href="Main.php"><h1>Security Suite</h1></a>
+            <span ontouchstart="openTouch(e)" onclick="openNav()"> 
+                <div class="navbg">
+                    <div class="bar1"></div>
 					<div class="bar2"></div>
 					<div class="bar3"></div>
 				</div>
@@ -21,51 +29,30 @@
 		<?php
 //		Checks user role and displays menu based on user role.
 		if($role == 1){ ?>
-	<div id="mySidenav" class="sidenav">
-		<a href="javascript:void(0)" class="closebtn" ontouchstart="closeTouch(e)"  onclick="closeNav()">&times;</a>
-  		<a href="Main.php">Home</a>
-  		<a href="account.php">Account</a>
-  		<a href="contacts.php">Contact</a>
-		<a href="index.php">Log Out</a>
-	</div>
-		
+            <div id="mySidenav" class="sidenav">
+                <a href="javascript:void(0)" class="closebtn" ontouchstart="closeTouch(e)"  onclick="closeNav()">&times;</a>
+                <a href="Main.php">Home</a>
+                <a href="account.php">Account</a>
+                <a href="contacts.php">Contact</a>
+                <a href="index.php">Log Out</a>
+            </div>
+        
 	<?php
-	} else if($role == 2)
-	{
-		?>
-	<div id="mySidenav" class="sidenav">
-		<a href="javascript:void(0)" class="closebtn" ontouchstart="closeTouch(e)"  onclick="closeNav()">&times;</a>
-  		<a href="Main.php">Home</a>
-		<a href="dashboard.php">Dashboard</a>
-  		<a href="account.php">Account</a>
-  		<a href="contacts.php">Contact</a>
-		<a href="index.php">Log Out</a>
-	</div>
+        } else if($role == 2){ ?>
+            <div id="mySidenav" class="sidenav">
+                <a href="javascript:void(0)" class="closebtn" ontouchstart="closeTouch(e)"  onclick="closeNav()">&times;</a>
+                <a href="Main.php">Home</a>
+                <a href="dashboard.php">Dashboard</a>
+                <a href="account.php">Account</a>
+                <a href="contacts.php">Contact</a>
+                <a href="index.php">Log Out</a>
+            </div>
 		
 		<?php
-	}
-	} else if($logged == false) { ?>
-			<div class="logo_div">
-			<a href="blog.php"><h1>Local Theatre</h1></a>
-			<span ontouchstart="openTouch(e)" onclick="openNav()"> 
-				<div class="navbg">
-					<div class="bar1"></div>
-					<div class="bar2"></div>
-					<div class="bar3"></div>
-				</div>
-			</span>
-		</div>
-		<div id="mySidenav" class="sidenav">
-			<a href="javascript:void(0)" class="closebtn" ontouchstart="closeTouch(e)" onclick="closeNav()">&times;</a>
-  			<a href="Main.php">Home</a>
-  			<a href="contacts.php">Contact</a>
-		</div>
-	
-	<?php
-	}
+	   }
+	} 
+
 	
 	?>
 
-	
 </div>
-
