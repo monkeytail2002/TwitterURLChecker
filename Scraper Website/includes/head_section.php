c8e894ecdfbf49710ef3ec43494707cfd83b50ac
Angus MacDonald <43272904+monkeytail2002@users.noreply.github.com>
2020-03-31 17:40:01 +0100

    Add files via upload
    Tidied up formatting and added commentary

--- Scraper Website/includes/head_section.php
+++ Scraper Website/includes/head_section.php
@@ -1,3 +1,9 @@
+<!--
+15009351 Angus MacDonald
+Tutor Suzanne Irvine
+31/3/2020
+-->
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,5 +16,5 @@
 	<meta name="viewport" content ="width=device-width, initial-scale=1">
 	<meta name ="description" content="">
 	<meta name="author" content="">
-	    <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.11/angular.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.11/angular.min.js"></script>
 
