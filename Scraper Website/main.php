852e4b9e0a48c28228b6bd5ee71ce6cf23476126
Angus MacDonald <43272904+monkeytail2002@users.noreply.github.com>
2020-04-06 17:11:12 +0100

    Add files via upload
    Added verify.php and main.php

---
+++ Scraper Website/main.php
@@ -0,0 +1,86 @@
+<!--Required modules-->
+
+<?php require_once('config.php'); ?>
+<?php require_once( ROOT_PATH . '/includes/head_section.php'); ?>
+
+<!--Checks if user session is valid-->
+<?php
+    if($_SESSION["Valid"]){
+    } else {
+        header("Location:index.php");
+    } ?>
+
+    <title>Twitter Search | Security Suite</title>
+</head>
+<body>
+    
+	<div class="container">
+        
+        <div class="banner">
+            <div class="welcome_msg">
+                <h1>Security Suite</h1>
+            </div>
+        </div>
+		
+						<!-- Navbar -->
+		<?php include( ROOT_PATH . '/includes/navbar.php'); ?>
+		
+		
+ 
+        <h1><center>Twitter Tool</center></h1>
+        <br><br>
+        <h2><center>Enter the term to search</center></h2>
+        <br>
+        <center><form action="twitter.php" method="post">
+            <input type="text" name="twitterSearch">
+        </form>
+        </center>
+    </div>
+
+
+    
+	<script>
+        
+	
+//	Script for opening the nav menu via click
+        try{
+            function openNav(){
+                document.getElementById("mySidenav").style.width = "250px";
+            }
+        } catch(openerr){
+            document.getElementById("mySidenav").innerHTML = openerr.message;
+        }
+
+//Script for closing the nav menu	
+        try{
+            function closeNav() {
+                document.getElementById("mySidenav").style.width = "0";
+            }
+        } catch(closeerr){
+            document.getElementById("mySidenav").innerHTML = closeerr.message;
+        }
+
+//script for making sure that the onclick doesn't default when ontouch triggers	for opening the nav menu
+        try {
+            function openTouch(e){
+                e.preventDefault();
+                e.target.onclick();
+            }
+        } catch(opTerr){
+            document.getElementById("mySidenav").innerHTML = opTerr.message;
+        }
+	
+//script for making sure that the onclick doesn't default when ontouch triggers	for closing the nav menu
+        try {
+            function closeTouch(e){
+                e.preventDefault();
+                e.target.onclick();
+            }
+        } catch(clTerr){
+            document.getElementById("mySidenav").innerHTML = clTerr.message;
+        }	
+	
+    </script>		
+    
+		<!-- Include Footer page -->
+<?php include('includes/footer.php') ?>
---
+++ Scraper Website/verify.php
@@ -0,0 +1,86 @@
+<?php require_once('config.php');?>
+
+
+<!DOCTYPE html>
+<html lang - "en">
+	<head>
+
+
+		<title>verify user</title>
+
+
+
+	</head>
+	
+	<body>
+<?php
+
+
+	 //Now get the information from the sign in form
+
+	 $Username	    = mysqli_real_escape_string($conn, $_POST['username']);
+	 $UserPassword  = mysqli_real_escape_string($conn, $_POST['password']);
+
+	 $Query = "SELECT * FROM Users WHERE Username = '$Username'";
+
+	 $Result = mysqli_query($conn,$Query);
+
+	 $NumResults = mysqli_num_rows($Result);	
+
+	 if ($NumResults==1)
+	 {
+
+
+	 	$Result = mysqli_query($conn,$Query);
+	 	$Row = mysqli_fetch_assoc($Result);
+        $UserId = $Row['UserId'];
+	 	$Email = $Row['Email'];
+	 	$UserName = $Row['Username'];
+        $HashedPassword = $Row['Password'];
+        $UserLevel = $Row['Role'];
+
+		 //Verify the password entered.
+                  
+         if (password_verify($UserPassword,$HashedPassword)){
+			 
+			 $_SESSION["UserId"] = $UserId;
+			 $_SESSION["Email"] = $Email;
+			 $_SESSION["UserName"] = $UserName;
+	 	 	 $_SESSION["Valid"] = 'True';
+         	 $_SESSION["UserLevel"] = $UserLevel;
+			 $_SESSION["HP"] = $HashedPassword;
+			 $_SESSION['susp'] = $Row['Suspended'];
+			 setcookie('Current_user',$UserName);
+			 
+				echo "<script type='text/javascript'>alert('Logging you in now');
+				window.location='main.php';
+				</script>";
+                 
+         }
+		 
+		 
+         else{
+			 echo "<script type='text/javascript'>alert('Incorrect username or password.  Please try again');
+				window.location='index.php';
+				</script>";
+	         }
+     }
+
+
+
+	 	else
+	 	{
+
+			echo "<script type='text/javascript'>alert('User not found');
+				window.location='index.php';
+				</script>";
+
+
+	 	}
+        
+        ?>
+
+    
+    
+    </body>
+    </html>
